e1fb05ce9ee77cdc19686b4e86303fbc2fd36eac
Daniel Zampar <danielszampa@gmail.com>
2019-05-18 14:34:56 -0300

    novos exercicios

--- Exercicios/ex072.py
+++ Exercicios/ex072.py
@@ -1,5 +1,10 @@
-name = input("Sua palavra:").strip()
-reverso = (name[::-1])
-print(f'Sua palavra é {name} e de traz para frente é {reverso}')
-print('teste')
-
+tupla = ('ZERO','UM','DOIS','TRES','QUATRO','CINCO',
+         'SEIS','SETE','OITO','NOVE','DEZ','ONZE','DOZE',
+         'TREZE','QUATORZE','QUINZE','DEZESSEIS','DEZESSETE',
+         'DEZOITO','DEZENOVE','VINTE')
+while True:
+    n = int(input('Digite um numero entre 0 e 20=>'))
+    if 0<= n <=20:
+        break
+    print('Digite o numero correto!!',end='...')
+print(f"Você digitou o numero {tupla[n]}")
---
+++ Exercicios/ex073.py
@@ -0,0 +1,9 @@
+times = ('Palmeiras','Santos','São Paulo','Atlético-MG','Botafogo',
+'Athletico-PR','Flamengo','Bahia','Internacional','Goiás',
+'Cruzeiro','Corinthians','Chapecoense','Ceará','Fluminense',
+'Fortaleza','CSA','Grêmio','Avaí','Vasco')
+print('='*80 + f'\n{"Tabela do Brasileiro 2019":^80}\n' + '=' *80)
+print(f'Os 5 primeiros colocados são {times[:5]}\n'
+      f'Os 4 ultimos colocados são {times[16:]}\n'
+      f'Os times em ordem alfabetica {sorted(times)}\n'
+      f'A chapecoense está em {times.index("Chapecoense")+1}')
